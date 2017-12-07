86d3f349a03da6c53f429369f22e8b83013228f3
echo3lima <eric_lobatos@yahoo.com>
2017-12-07 11:40:10 +0000

    Fixed js linkage to homepage  error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,37 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+/* global $ */
+
+$(document).ready(function() {
+    
+    $('.portfolio').waypoint(function(direction) { 
+    if (direction === 'down') {
+      $('.pic-bg').removeClass('invisble');
+      $('.devmatch-text').removeClass('invisble');
+    } else {
+      $('.pic-bg').addClass('invisble');
+      $('.devmatch-text').addClass('invisble');
+    }
+      }, { offset: '30%' }
+    );
+  
+    $('.fa-envelope').click(function() { 
+      $('.email').fadeIn('slow');
+    });
+    setBindings('a[href^="#"]');
+    setBindings('.el-logo');
+  
+  
+    function setBindings(target) {
+        $(target).click(function(e) {
+          e.preventDefault();
+          let sectionID = e.currentTarget.id + "Section";
+          
+          $("html, body").animate({
+            scrollTop: $("#" + sectionID).offset().top
+          }, 1000);
+        });
+    };
+});
